502a5d614cee367a81d997ca1f4b91332072cca0
Kezuu-Afk <your-email@example.com>
2025-11-09 00:41:04 +0530

    Fix locale routing issue

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,9 @@ import createMiddleware from "next-intl/middleware";
 export default createMiddleware({
    locales: ["en", "nl"],
    defaultLocale: "en",
+   localePrefix: "as-needed",
 });
 
 export const config = {
-   matcher: ["/", "/(nl|en)/:path*"],
+   matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
 };
