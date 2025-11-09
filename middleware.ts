dbfc093b9ecf79e40e8655a2847ba381a3ffbe72
Kezuu-Afk <your-email@example.com>
2025-11-09 10:31:00 +0530

    Remove vercel.json and simplify middleware

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,6 @@ import createMiddleware from "next-intl/middleware";
 export default createMiddleware({
    locales: ["en", "nl"],
    defaultLocale: "en",
-   localePrefix: "always",
-   localeDetection: true,
 });
 
 export const config = {
